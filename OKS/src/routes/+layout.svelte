378909b58bea0383d30aa3ef5fb91681cea3d32f
VageeshHegde <vageeshdhegde99@gmail.com>
2025-09-14 20:00:37 -0400

    Fix styling: Move Bootstrap to app.html, disable Tailwind conflicts

--- OKS/src/routes/+layout.svelte
+++ OKS/src/routes/+layout.svelte
@@ -22,12 +22,6 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
-	<!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-	<!-- Font Awesome -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
-	<!-- Bootstrap JS -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
 </svelte:head>
 
 {@render children?.()}
