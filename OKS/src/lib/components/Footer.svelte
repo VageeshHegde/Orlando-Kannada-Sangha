135dbeefe28b754b0d93d41cc91a30723d476033
VageeshHegde <vageeshdhegde99@gmail.com>
2025-09-21 17:53:22 -0400

    Add login link to footer: Included a new "Login" link in the footer for easier access to the login page, enhancing navigation for users.

--- OKS/src/lib/components/Footer.svelte
+++ OKS/src/lib/components/Footer.svelte
@@ -70,6 +70,7 @@
             <li><a href="/blog"><i class="fas fa-blog"></i>OKS Blog</a></li>
             <li><a href="/constitution"><i class="fas fa-gavel"></i>Constitution</a></li>
             <li><a href="/by-laws"><i class="fas fa-book-open"></i>By-Laws</a></li>
+            <li><a href="/login"><i class="fas fa-sign-in-alt"></i>Login</a></li>
             <li><a href="https://www.zeffy.com/en-US/donation-form/e9e95cc9-b449-49eb-85c8-cf5efa4a712e" target="_blank" rel="noopener noreferrer"><i class="fas fa-hand-holding-dollar"></i>Donate</a></li>
           </ul>
         </div>
